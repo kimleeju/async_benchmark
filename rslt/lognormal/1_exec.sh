51b4486db705f7406c9645c30307c7ea1c94fe80
kimleeju <kimleeju@gmail.com>
2022-04-15 20:10:00 +0900

    added lognormal graph

--- rslt/lognormal/1_exec.sh
+++ rslt/lognormal/1_exec.sh
@@ -1,5 +1,5 @@
-FILE="lognormal_0.1 lognormal_0.5 lognormal_1"
-OFILE=lognormal.txt
+FILE="lognormal_0.1 lognormal_0.25 lognormal_0.5"
+OFILE="lognormal.txt"
 
 TFILE=temp.tmp
 n=0
@@ -10,10 +10,10 @@ done
 
 for FI in $FILE; do
 	start=(`head -n 1 $FI.txt` + 0 )
-	cat $FI.txt | awk '{printf("%.6f\n", ($1-'"$start"')/1000000)}' > $n.tmp
+	cat $FI.txt | awk '{printf("%.9f\n",$1/1000)}' | sort -n  > $n.tmp
 	n=$(($n + 1))
 done
 
-echo "percent S=0.1 S=0.5 L=1" > $OFILE 
+echo "percent S=1 S=10 S=50" > $OFILE 
 paste $TFILE 0.tmp 1.tmp 2.tmp >> $OFILE
 rm *.tmp
